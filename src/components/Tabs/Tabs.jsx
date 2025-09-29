b96f8665eddd8af7da2b86b2e26b87028b7786dd
Matvey Turluk <matychencia.ua@gmail.com>
2025-09-29 16:58:46 +0300

    added task solution with one issue

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,43 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  // const getTargetsValue = event => {
+  //   return event.target.dataset.key;
+  // };
+
+  return (
+    <div className="section">
+      <h1 className="title">
+        {`Selected tab is ${tabs.find(tab => tab.id === activeTabId)?.title}`}
+      </h1>
+
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                key={tab.id}
+                data-key={tab.id}
+                className={tab.id === activeTabId ? 'is-active' : ''}
+                data-cy="Tab"
+                onClick={event => {
+                  const tabId = event.currentTarget.dataset.key;
+
+                  if (tabId !== activeTabId) {
+                    onTabSelected(tabId);
+                  }
+                }}
+              >
+                <a href={`#${tab.id}`} data-cy="TabLink">
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="block" data-cy="TabContent">
+          {tabs.find(tab => tab.id === activeTabId)?.content}
+        </div>
+      </div>
+    </div>
+  );
+};
